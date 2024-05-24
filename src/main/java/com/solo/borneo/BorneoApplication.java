9fb6f984c4f3e3505172ecad878d94a01b32d17a
Wei Jin Luo <weijinluo@gmail.com>
2024-05-24 18:18:06 -0400

    “first-commit”

---
+++ src/main/java/com/solo/borneo/BorneoApplication.java
@@ -0,0 +1,13 @@
+package com.solo.borneo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorneoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BorneoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solo/borneo/BorneoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solo.borneo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorneoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
